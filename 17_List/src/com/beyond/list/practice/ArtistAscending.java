1cf71e9f6811afe1dab7ecd703771ee46c698008
jelee55 <jegyeong25@gmail.com>
2025-01-13 17:48:40 +0900

    20250113_[API, Collection}

---
+++ 17_List/src/com/beyond/list/practice/ArtistAscending.java
@@ -0,0 +1,11 @@
+package com.beyond.list.practice;
+
+import java.util.Comparator;
+
+public class ArtistAscending implements Comparator<Music> {
+
+    @Override
+    public int compare(Music o1, Music o2) {
+        return 0;
+    }
+}
