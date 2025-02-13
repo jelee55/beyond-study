8a272463df07fac30a19c5798c2e82a5fcc8829d
jelee55 <jegyeong25@gmail.com>
2025-02-13 17:39:11 +0900

    20250213_주석수정

--- 02_SpringAOP/src/main/java/com/beyond/aop/config/RootConfig.java
+++ 02_SpringAOP/src/main/java/com/beyond/aop/config/RootConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@ComponentScan("com.beyond.aop") // com.beyond.aop 하위 모든 componeent 어노테이션이 확인후 bean으로 등록시켜줌
+@ComponentScan("com.beyond.aop") // ComponentScan을 통해 com.beyond.aop 하위 모든 componeent 어노테이션을 찾아서 bean으로 등록시켜줌
 @EnableAspectJAutoProxy
 public class RootConfig {
 
--- 02_SpringAOP/src/test/java/com/beyond/aop/character/CharacterTest.java
+++ 02_SpringAOP/src/test/java/com/beyond/aop/character/CharacterTest.java
@@ -13,7 +13,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// 단위 테스트에 공통적으로 사용할 확장 기능을 선언해주는 역할
 @ExtendWith(SpringExtension.class)
+// 생성된 스프링 컨테이너에 스프링 빈을 추가하기 위해서는 application-context.xml 파일과 같은 설정 파일을 읽어야 하는데, 이런 설정파일을 로드하는 어노테이션
 @ContextConfiguration(classes = RootConfig.class)
 class CharacterTest {
 
